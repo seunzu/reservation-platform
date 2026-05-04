d7edee388e09898585c3a070b41a18c0ac9a351b
seunzu <0123suh@gmail.com>
2026-05-04 12:57:05 +0900

    chore: init project

---
+++ src/test/java/com/reservation/platform/PlatformApplicationTests.java
@@ -0,0 +1,14 @@
+package com.reservation.platform;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+class PlatformApplicationTests {
+
+	@Test
+	void applicationClassExists() {
+		assertNotNull(PlatformApplication.class);
+	}
+
+}
